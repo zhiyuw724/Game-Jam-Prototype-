ec9c42af12a857e151c65c6b337142b4730661ac
guptaaks7 <122641035+guptaaks7@users.noreply.github.com>
2023-09-21 18:22:08 -0700

    Update moveFishSlow.cs

--- Assets/moveFishSlow.cs
+++ Assets/moveFishSlow.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class moveFishSlow : MonoBehaviour
 {
-    public float speed = 2.0f;
+    public float speed = 2.5f;
+    public float rightBorder = 4.0f; 
+    public float leftBorder = -4.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +17,13 @@ public class moveFishSlow : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (transform.position.x >= rightBorder)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+        // else if (transform.position.x == leftBorder)
+        // {
+        //     transform.rotation = Quaternion.Euler(0, 0, 0);
+        // }
     }
 }
