24c872c2ea46dcfb23845f948605f6e4b27fca48
guptaaks7 <122641035+guptaaks7@users.noreply.github.com>
2023-09-21 17:33:01 -0700

    Slow fish movement

--- Assets/moveFishSlow.cs
+++ Assets/moveFishSlow.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveRight : MonoBehaviour
+public class moveFishSlow : MonoBehaviour
 {
-    public float speed = 1.0f;
+    public float speed = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +14,6 @@ public class moveRight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
     }
 }
